dab51918ef376ae6ced595824307e5c57c354895
Tamer Osman <tamer4osman@gmail.com>
2023-07-20 01:14:53 +0200

    load and store in localsorage with hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  const [todos, setTodos] = useState(initialTodos);
+
+  // Load todos from localStorage on component mount
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    setTodos(storedTodos);
+  }, []);
+
+  // Save todos to localStorage whenever 'todos' changes
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+
 
   return (
-    <>
-
-    </>
-  )
-}
+    <div>
+      
+    </div>
+  );
+};
 
 export default App
